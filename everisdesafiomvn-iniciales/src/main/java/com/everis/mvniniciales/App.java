6148e6d02eeb95705bbfff2c87291d8a00072921
quijano174 <quijano174@gmail.com>
2021-10-25 14:26:27 +0200

    the first commit

---
+++ everisdesafiomvn-iniciales/src/main/java/com/everis/mvniniciales/App.java
@@ -0,0 +1,13 @@
+package com.everis.mvniniciales;
+
+import org.apache.commons.lang3.*;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        StringUtils.isBlank(" ");
+    }
+    
+    
+}
